ed58c2a9697de9b77fcc345285422c705a78b20f
Artem Shcheglov <stiw00@bk.ru>
2017-12-03 02:12:09 +0300

    all work

---
+++ lab4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <semaphore.h>
+
+
+int main()
+{
+	printf("Hello World!");
+  return 0;
+}
